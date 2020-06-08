8db174740232f7c741fb4be6150f1e0fb5867478
jaehun <dybe15@gmail.com>
2020-06-09 05:14:26 +0900

    Posts 등록 API 테스트

--- src/main/java/com/dybe/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/dybe/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,7 @@
 package com.dybe.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
