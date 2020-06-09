6266e7d9d7b82c6685757761864e722192508f4a
jaehun <dybe15@gmail.com>
2020-06-09 13:37:46 +0900

    Posts 등록 API 테스트

--- src/main/java/com/dybe/springboot/service/posts/PostsService.java
+++ src/main/java/com/dybe/springboot/service/posts/PostsService.java
@@ -2,7 +2,9 @@ package com.dybe.springboot.service.posts;
 
 import com.dybe.springboot.domain.posts.Posts;
 import com.dybe.springboot.domain.posts.PostsRepository;
+import com.dybe.springboot.web.dto.PostsResponseDto;
 import com.dybe.springboot.web.dto.PostsSaveRequestDto;
+import com.dybe.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -19,6 +21,20 @@ import javax.transaction.Transactional;
         @Transactional
         public Long save(PostsSaveRequestDto requestDto){
             return postsRepository.save(requestDto.toEntity()).getId();
+        }
+
+        @Transactional
+        public Long update(Long id, PostsUpdateRequestDto requestDto){
+            Posts posts = postsRepository.findById(id)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+            posts.update(requestDto.getTitle(), requestDto.getContent());
+            return id;
+        }
+
+        public PostsResponseDto findById(Long id) {
+            Posts entity = postsRepository.findById(id)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+            return new PostsResponseDto(entity);
         }
     }
 
