c995044b3f5024620d9e25cbe6da4ecf3df03258
jaehun <dybe15@gmail.com>
2020-06-09 13:52:29 +0900

    등록/수정/조회 기능 제작 및 테스트

---
+++ src/main/java/com/dybe/springboot/web/PostsApiController.java
@@ -0,0 +1,29 @@
+package com.dybe.springboot.web;
+
+import com.dybe.springboot.service.posts.PostsService;
+import com.dybe.springboot.web.dto.PostsResponseDto;
+import com.dybe.springboot.web.dto.PostsSaveRequestDto;
+import com.dybe.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
