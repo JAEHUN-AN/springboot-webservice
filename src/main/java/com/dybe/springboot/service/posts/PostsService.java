4e227714fda6b119ace4953389956996fd081f8e
jaehun <dybe15@gmail.com>
2020-06-22 14:43:01 +0900

    게시글 수정기능

--- src/main/java/com/dybe/springboot/service/posts/PostsService.java
+++ src/main/java/com/dybe/springboot/service/posts/PostsService.java
@@ -2,12 +2,14 @@ package com.dybe.springboot.service.posts;
 
 import com.dybe.springboot.domain.posts.Posts;
 import com.dybe.springboot.domain.posts.PostsRepository;
+import com.dybe.springboot.web.dto.PostsListResponseDto;
 import com.dybe.springboot.web.dto.PostsResponseDto;
 import com.dybe.springboot.web.dto.PostsSaveRequestDto;
 import com.dybe.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
@@ -20,21 +22,28 @@ import javax.transaction.Transactional;
 
         @Transactional
         public Long save(PostsSaveRequestDto requestDto){
+
             return postsRepository.save(requestDto.toEntity()).getId();
         }
 
         @Transactional
         public Long update(Long id, PostsUpdateRequestDto requestDto){
             Posts posts = postsRepository.findById(id)
-                    .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+                    .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
             posts.update(requestDto.getTitle(), requestDto.getContent());
             return id;
         }
 
         public PostsResponseDto findById(Long id) {
             Posts entity = postsRepository.findById(id)
-                    .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+                    .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
             return new PostsResponseDto(entity);
+        }
+
+        public List<PostsListResponseDto> findAllDesc(){
+            return postsRepository.findAllDesc().stream()
+                    .map(PostsListResponseDto::new)
+                    .collect(Collectors.toList());
         }
     }
 
